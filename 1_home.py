dfb6060c051233bfcb18f3a762cdde630bbf53b4
luanayress <111098033+luanayress@users.noreply.github.com>
2024-09-13 11:38:24 -0300

    Update 1_home.py
    incluindo pacotes pra importação

--- 1_home.py
+++ 1_home.py
@@ -2,6 +2,9 @@ import numpy as np
 from datetime import date
 import pandas as pd
 import streamlit as st
+import random
+import plotly.graph_objects as go
+import yfinance as yf
 
 acao_escolhida = st.sidebar.text_input("Digite a Ação", value="AAPL")
 
